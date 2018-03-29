614ba0a02b3a9849af3e5fba6a43edf50dceb82d
Cristian Moraru <cristian.moraru@live.com>
2018-03-29 16:54:56 +0200

    Add project files.

---
+++ RadioBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RadioBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
